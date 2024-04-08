7dfa33c2b469d14ae0ac11ef912f0410949b6c8d
Guerizim <gueri_leon@outlook.com>
2024-04-08 13:25:31 -0700

    Add Art route

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Art, Medium, Style, Comment
 # Register your models here.
+admin.site.register(Art)
+admin.site.register(Medium)
+admin.site.register(Style)
+admin.site.register(Comment)
--- main_app/models.py
+++ main_app/models.py
@@ -5,43 +5,42 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 MEDIUM = (
-    ('Digital'),
-    ('Mural'),
-    ('Water Color'),
-    ('Pastel'),
-    ('Acrylic'),
-    ('Oil'),
-    ('Ink'),
-    ('Print Making'),
-    ('Charcoal'),
-    ('Sumi Ink'),
-    ('Photography'),
-    ('Sculpture'),
-    ('Mix Media'),
-    ('Collage'),
-    ('Others')
+    ('DIG','Digital'),
+    ('MUR','Mural'),
+    ('H2O', 'Water Color'),
+    ('PAST', 'Pastel'),
+    ('CRY', 'Acrylic'),
+    ('OIL', 'Oil'),
+    ('INK', 'Ink'),
+    ('PRI','Print Making'),
+    ('CHA','Charcoal'),
+    ('PHO', 'Photography'),
+    ('SCU', 'Sculpture'),
+    ('MIX', 'Mixed Media'),
+    ('COL', 'Collage'),
+    ('OTH', 'Others')
 )
 
 STYLES = (
-    ('Impressionism'),
-    ('Cubism'),
-    ('Surrealism'),
-    ('Realism'),
-    ('Abstract'),
-    ('Romanticism'),
-    ('Art Nouveau'),
-    ('Pop Art'),
-    ('Post Modernism'),
-    ('Minimalism'),
-    ('Contemporary'),
-    ('Thangka'),
-    ('Others')
+    ('IMP','Impressionism'),
+    ('CUB','Cubism'),
+    ('SUR', 'Surrealism'),
+    ('REA', 'Realism'),
+    ('ABS','Abstract'),
+    ('ROM', 'Romanticism'),
+    ('ART', 'Art Nouveau'),
+    ('POP', 'Pop Art'),
+    ('pos','Post Modernism'),
+    ('MIN', 'Minimalism'),
+    ('CON', 'Contemporary'),
+    ('THA', 'Thangka'),
+    ('OTH', 'Others')
 )
 
 
 # Model 3: Medium
 class Medium(models.Model):
-    name = models.CharField(max_length=75, choices=MEDIUM, default=MEDIUM[0[0]])
+    name = models.CharField(max_length=75, choices=MEDIUM, default=MEDIUM[0][0])
 
     def __str__(self):
         return self.name
@@ -52,7 +51,7 @@ class Medium(models.Model):
 
 # Model 2: Style
 class Style(models.Model):
-    name = models.CharField(max_length=75, choices=STYLES, default=STYLES[0[0]])
+    name = models.CharField(max_length=75, choices=STYLES, default=STYLES[0][0])
 
     def __str__(self):
         return self.name
@@ -65,8 +64,8 @@ class Art(models.Model):
     title = models.CharField(max_length=75)
     image = models.CharField(max_length=500)
     description = models.TextField(max_length=750)
-    date = models.DateField('Art Creation Date')
-    price = models.DecimalField(max_digits=10000, decimal_places=2)
+    # date = models.DateField('Art Creation Date')
+    price = models.DecimalField(max_digits=1000, decimal_places=2)
     like = models.IntegerField(default=0)
     style = models.ManyToManyField(Style)
     medium = models.ManyToManyField(Medium)
@@ -75,8 +74,8 @@ class Art(models.Model):
     def __str__(self):
         return f'{self.title} ({self.id})'
     
-    def __str__(self):
-        return f"{self.XXX()} on {self.date}"
+    # def __str__(self):
+    #     return f"{self.()} on {self.date}"
     
     def get_absolute_url(self):
         return reverse('artworks_detail', kwargs={'art_id': self.id})
@@ -85,7 +84,7 @@ class Art(models.Model):
 
 # Model 4: Comment
 class Comment(models.Model):
-    date = models.DateField()
+    # date = models.DateField("Comment Posting Date")
     comment = models.TextField(max_length=280)
 
   # Create an art_id FK
@@ -93,6 +92,9 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'Comment by {self.user.username} on {self.date}'
+
+    # class Meta:
+    #     ordering = ['-date']
 
 
     
