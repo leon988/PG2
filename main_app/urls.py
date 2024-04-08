1c340c9c0fabdaf21664aca58699df8829ff8919
Ganesh <ganeshrestha97@gmail.com>
2024-04-08 14:05:16 -0700

    added class art, defined home and about in views.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -12,19 +12,24 @@ urlpatterns = [
   path('art/<int:pk>/update/', views.ArtUpdate.as_view(), name='arts_update'),
   path('art/<int:pk>/delete/', views.ArtDelete.as_view(), name='arts_delete'),
 
-  # Model 2 - Style CRUD
+  # Model 2 - Style RUD
   path('style/', views.StyleList.as_view(), name='styles_index'),
   path('style/<int:pk>/', views.StyleDetail.as_view(), name='styles_detail'),
   path('style/<int:pk>/update/', views.StyleUpdate.as_view(), name='styles_update'),
   path('style/<int:pk>/delete/', views.StyleDelete.as_view(), name='styles_delete'),
 
-  # Model 3 - Medium CRUD
-  path('medium/', views.MediumList.as_view(), name='medium_index'),
+  # Model 3 - Medium RUD
+  path('medium/', views.MediumList.as_view(), name='mediums_index'),
   path('medium/<int:pk>/', views.MediumDetail.as_view(), name='mediums_detail'),
   path('medium/<int:pk>/update/', views.MediumUpdate.as_view(), name='mediums_update'),
   path('medium/<int:pk>/delete/', views.MediumDelete.as_view(), name='mediums_delete'),
 
   # Model 4 - Comment CRUD
+  path('comment/', views.CommentList.as_view(), name='comments_index'),
+  path('comment/<int:pk>/', views.CommentDetail.as_view(), name='comments_detail'),
+  path('comment/create/', views.CommentCreate.as_view(), name='comments_create'),
+  path('comment/<int:pk>/update/', views.CommentUpdate.as_view(), name='comments_update'),
+  path('comment/<int:pk>/delete/', views.CommentDelete.as_view(), name='comments_delete'),
 
 
   # Model 5 - User
